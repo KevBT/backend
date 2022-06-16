9384bae0d5ba79316a73f4bf0d8d44a347d17da8
Kevin Esteban Baron Perez <kevin.baron@educar.com.co>
2022-06-16 11:18:58 -0500

    variables de entorno agregados

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
 const DB = require("mysql");
+require('dotenv').config()
 
 const connection = DB.createConnection({
   host: "mf1l4m8uvc3f.us-east-3.psdb.cloud",
-  user: "lq9zc21v2j7c",
-  password: "pscale_pw_BPGfAOTOJkQDSlfml19OZnXSkBGReBtvZy1XGNQG2Wc",
+  user: process.env.SECRET_USER,
+  password: process.env.SECRET_KEY,
   database: "juegosalianzas",
   ssl: {
     rejectUnauthorized: false
